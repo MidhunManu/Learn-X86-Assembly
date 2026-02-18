1fa7e0a8eb437788cbb3a35e988422a6e3089f7e
MidhunManu <midhunmanu384@gmail.com>
2026-02-18 15:47:14 +0530

    refactor: define gobal strnlen function

--- libc_impl/strlen.s
+++ libc_impl/strlen.s
@@ -1,26 +1,7 @@
 .intel_syntax noprefix
-.global _start
 .global strlen
 
-.section .bss
-buffer: .skip 16
-
 .section .text
-_start:
-    mov rax, 0
-    mov rdi, 0
-    lea rsi, buffer
-    mov rdx, 16
-    syscall
-
-    mov rsi, rax
-    lea rdi, buffer
-    call strlen
-
-    mov rax, 60
-    xor rdi, rdi
-    syscall
-
 strlen:
     mov rcx, rdi
     xor r8, r8
